2657648a71b60261454017f8aff49acdda36aec8
PalmNapalm <PalmNapalm@no-reply.com>
2024-12-26 15:46:49 -0500

    add simple emotion detect

---
+++ emotion_detection.py
@@ -0,0 +1,31 @@
+# Example usage:
+# > python3.11
+# > from emotion_detection import emotion_detector
+# > print(emotion_detector("I hate working long hours"))
+
+import requests
+
+def emotion_detector(text_to_analyze):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"
+    }
+    input_json = {
+        "raw_document": {
+            "text": text_to_analyze
+        }
+    }
+    
+    response = requests.post(url, headers=headers, json=input_json)
+    
+    if response.status_code == 200:
+        result = response.json()
+        emotions = result['emotionPredictions'][0]['emotion']
+        
+        # Find the emotion with the highest score
+        dominant_emotion = max(emotions, key=emotions.get)
+        dominant_score = emotions[dominant_emotion]
+        
+        return dominant_emotion
+    else:
+        return f"Error: {response.status_code}"
